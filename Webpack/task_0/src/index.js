3aaac9a7cf73ebcf6bc5f9b13c18c57929160e68
daeshov <jdarahthomas@gmail.com>
2024-01-02 17:34:07 -0600

    task 0

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+import $ from 'jquery';
+
+$(document).ready(function () {
+  // Add three paragraphs to the body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
