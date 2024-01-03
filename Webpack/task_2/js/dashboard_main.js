7b8814d53a293162442de9f57822637be80bf8c8
daeshov <jdarahthomas@gmail.com>
2024-01-02 19:48:42 -0600

    task 2

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$(document).ready(function () {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="startButton">Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+  const updateCounter = () => {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  };
+
+  // Debounce click event using Lodash
+  const debouncedUpdateCounter = _.debounce(updateCounter, 1000);
+
+  // Bind click event to button
+  $('#startButton').on('click', debouncedUpdateCounter);
+});
---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+// webpack.config.js
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+};
