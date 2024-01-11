326e587564f2c4cfcfb6e35bdef7727960b914f9
daeshov <jdarahthomas@gmail.com>
2024-01-10 22:32:09 -0600

    task1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Notifications from '../Notifications/Notifications.js';
 import Header from '../Header/Header.js';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 
-function App() {
-  return (
-    <React.Fragment>
-    <Notifications displayDrawer={true} notifications={[]} />
-    <div className='App-body'>
-      <Header />
-      <Login />
-      <Footer />
-    </div>
-    </React.Fragment>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: props.isLoggedIn || false,
+    };
+    this.logOut = props.logOut;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Notifications displayDrawer={true} notifications={[]} />
+        <div className='App-body'>
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
