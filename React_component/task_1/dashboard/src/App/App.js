be2cfdcca92dce3cca09bed87209e6eaa279a3f8
daeshov <jdarahthomas@gmail.com>
2024-01-10 22:41:42 -0600

    task 1

--- React_component/task_1/dashboard/src/App/App.js
+++ React_component/task_1/dashboard/src/App/App.js
@@ -8,17 +8,6 @@ import PropTypes from 'prop-types';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-  {id: 1, name: "ES6", credit: 60},
-  {id: 2, name: "Webpack", credit: 20},
-  {id: 3, name: "React", credit: 40}
-]
-
-const listNotifications = [
-  {id: 1, type: "default", value: "New course available"},
-  {id: 2, type: "urgent", value: "New resume available"},
-  {id: 3, type: "urgent", __html: {__html: getLatestNotification()}},
-]
 
 class App extends React.Component {
   constructor(props) {
