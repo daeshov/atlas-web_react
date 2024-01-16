afc791f4f71ba06d37e1e27be8191dc64332f805
daeshov <jdarahthomas@gmail.com>
2024-01-15 18:53:29 -0600

    task 1

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -2,8 +2,13 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BodySectionWithMarginBottom from "./BodySectionWithMarginBottom";
 
-describe("TEST SUIT", () => {
-  it("TEST CASE 1: checking that shallowing the component should render correctly a BodySection component and that the props are passed correctly to the child component", () => {
+describe('<BodySectionWithMarginBottom />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
     render(
       <BodySectionWithMarginBottom title="test title">
         <p>test children</p>
@@ -13,5 +18,4 @@ describe("TEST SUIT", () => {
     expect(screen.getByTestId("bodySectionWithMargin")).toBeInTheDocument(); // Use getByTestId for the wrapper
     expect(screen.getByTestId("BodySection").textContent).toContain("test title"); // Use getByTestId for the inner BodySection
     expect(screen.getByText("test children")).toBeInTheDocument(); // Use getByText for checking text content
-  });
 });
