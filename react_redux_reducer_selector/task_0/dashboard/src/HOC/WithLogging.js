b5cf914862480f4cb66a5bc0cb112bffed1b5ddd
daeshov <jdarahthomas@gmail.com>
2024-01-29 11:09:33 -0600

    task 0

---
+++ react_redux_reducer_selector/task_0/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,23 @@
+import React, { Component, Children } from 'react';
+
+class WithLogging extends Component {
+	constructor(props) {
+		super(props);
+	};
+
+	componentDidMount() {
+		let compName = this.props.children.type.name || 'Component';
+		console.log(`Component ${compName} is mounted`);
+	};
+
+	componentWillUnmount() {
+		let compName = this.props.children.type.name || 'Component';
+		console.log(`Component ${compName} is going to unmount`);
+	};
+
+	render() {
+		return (this.props.children);
+	};
+};
+
+export default WithLogging;
